9b7786560f2f00711f99e177cfaa6acd37106ecc
Jordon Marchesano <jordonomarchesano@gmail.com>
2023-10-15 12:01:54 -0700

    font to monserrat

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () =>{
 
     return(
         <div className="container mx-auto  px-20 pb-32 text-center lg:mx-auto lg:px-60">
-            <p className="text-xl font-sans">Sus Af is an authentic skateboarding brand rooted in Los Angeles and run entirely by passionate local skateboarders. Our mission is to deliver the utmost quality products crafted right here in California. While being based in the Golden State might lead you to assume higher prices, we have successfully streamlined our shipping and importation processes, allowing us to offer superior products at a competitive price point.
+            <p className="text-xl ">Sus Af is an authentic skateboarding brand rooted in Los Angeles and run entirely by passionate local skateboarders. Our mission is to deliver the utmost quality products crafted right here in California. While being based in the Golden State might lead you to assume higher prices, we have successfully streamlined our shipping and importation processes, allowing us to offer superior products at a competitive price point.
         </p>
         </div>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () =>{
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-black mb-3">
               <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                 <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                    <Link className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white" to="/">sus af skateboards</Link>
+                    <Link className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white" to="/"><p className="nav-text">sus af skateboards</p></Link>
                   <button
                     className="text-white cursor-pointer text-xl leading-none px-1 py-.5 border border-solid border-transparent rounded bg-transparent bg-black block lg:hidden outline-none focus:outline-none"
                     type="button"
@@ -32,20 +32,20 @@ const Header: React.FC = () =>{
                 >
                   <ul className="flex flex-col lg:flex-row list-none lg:ml-auto ">
                     <li className="nav-item">
-                      <Link className="px-3 py-2 justify-end flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/skateboards">skateboards</Link>
+                      <Link className="px-3 py-2 justify-end flex items-center text-xs  font-bold leading-snug text-white hover:opacity-75" to="/skateboards"><p className="nav-text">skateboards</p></Link>
                                    
                     </li>
                     <li className="nav-item ">
-                     <Link className="px-3 py-2 flex justify-end items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/shirts">shirts</Link>
+                     <Link className="px-3 py-2 flex justify-end items-center text-xs  font-bold leading-snug text-white hover:opacity-75" to="/shirts"><p className="nav-text">shirts</p></Link>
             
                     </li>
                     <li className="nav-item">
-                     <Link className="px-3 py-2 flex justify-end items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/pants">pants</Link>
+                     <Link className="px-3 py-2 flex justify-end items-center text-xs  font-bold leading-snug text-white hover:opacity-75" to="/pants"><p className="nav-text">pants</p></Link>
                   
                     </li>
                 
                     <li className="nav-item">
-                      <Link className="px-3 py-2 flex justify-end items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" to="/cart"> 
+                      <Link className="px-3 py-2 flex justify-end items-center text-xs  font-bold leading-snug text-white hover:opacity-75" to="/cart"> 
                       <FontAwesomeIcon icon={faCartShopping}/>
                       </Link>
                     </li>
